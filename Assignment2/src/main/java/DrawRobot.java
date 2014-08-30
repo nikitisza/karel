f2e7321940d0eed7acdd5c401c0bdca20a53a42f
Niki <nikolett.tisza@gmail.com>
2014-08-31 10:11:44 +1200

    DrawRobot solution

---
+++ Assignment2/src/main/java/DrawRobot.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Niki on 30/08/2014.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+
+import java.awt.*;
+
+public class DrawRobot extends GraphicsProgram {
+    public void run() {
+
+        GOval head = new GOval(300, 80, 90, 90);
+           head.setFillColor(Color.RED);
+           head.setFilled(true);
+           head.setColor(Color.red);
+           add(head);
+
+           add(new GRect(250, 170, 190, 210));
+           add(new GRect(285, 380, 30, 100));
+           add(new GRect(375, 380, 30, 100));
+    }
+}
