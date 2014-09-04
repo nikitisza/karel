3c3ca316d54b41146ac9cd1f4fd9eea5ee3d7447
Niki <nikolett.tisza@gmail.com>
2014-09-04 14:31:39 +1200

    target

--- Assignment2/src/main/java/Target.java
+++ Assignment2/src/main/java/Target.java
@@ -14,15 +14,25 @@ public class Target extends GraphicsProgram {
     double radiusOuter = 72;
     double radiusMiddle = radiusOuter*0.65;
     double radiusInner = radiusOuter*0.3;
-    int yMiddlePoint = getHeight()/2;
-    int xMiddlePoint = getWidth()/2;
+    double yMiddlePoint = getHeight()/2;
+    double xMiddlePoint = getWidth()/2;
 
 	public void run()  {
 
-        GOval circle = new GOval(xMiddlePoint + radiusOuter, yMiddlePoint + radiusOuter, 2 * radiusOuter, 2 * radiusOuter);
+        GOval circle = new GOval(xMiddlePoint-radiusOuter, yMiddlePoint-radiusOuter, 2 * radiusOuter, 2 * radiusOuter);
         circle.setFilled(true);
         circle.setColor(Color.RED);
         add(circle);
+
+        GOval circleMiddle = new GOval(xMiddlePoint-radiusMiddle, yMiddlePoint-radiusMiddle, 2* radiusMiddle, 2 * radiusMiddle);
+        circleMiddle.setFilled(true);
+        circleMiddle.setColor(Color.white);
+        add(circleMiddle);
+
+        GOval circleInner = new GOval(xMiddlePoint-radiusInner, yMiddlePoint-radiusInner, 2*radiusInner, 2*radiusInner);
+        circleInner.setFilled(true);
+        circleInner.setColor(Color.RED);
+        add(circleInner);
 
   }
 
