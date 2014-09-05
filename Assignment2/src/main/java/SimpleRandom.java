2625ce79e6e07d2afc5808a4993235f0bbc34bde
Niki <nikolett.tisza@gmail.com>
2014-09-06 10:13:47 +1200

    extended simple random

--- Assignment2/src/main/java/SimpleRandom.java
+++ Assignment2/src/main/java/SimpleRandom.java
@@ -6,9 +6,29 @@ import acm.util.*;
  */
 public class SimpleRandom extends ConsoleProgram {
 
+    private static final int DICE_SIDES = 6;
+
     public void run() {
-        int diceRoll = myRgen.nextInt(1, 6);
-        println("Your lucky number is: " + diceRoll);
+        int numberOfDice = readInt("How many dice do you have?: ");
+        int maximumRolledNumber = numberOfDice * DICE_SIDES;
+        int rollCounter = 0;
+
+        while (true) {
+            int roll = rollDice(numberOfDice);
+            rollCounter++;
+            if (roll == maximumRolledNumber) break;
+            println("Rolled: " + roll);
+        }
+        println("You had to roll: " + rollCounter + " to get " + maximumRolledNumber);
+    }
+
+    private int rollDice(int nOD){
+        int total = 0;
+
+        for (int i=0; i<nOD; i++) {
+            total += myRgen.nextInt(1,DICE_SIDES);
+        }
+        return total;
     }
 
     private RandomGenerator myRgen = RandomGenerator.getInstance();
