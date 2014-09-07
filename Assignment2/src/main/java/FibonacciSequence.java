da76c9d727510a73201570feb243f8e260d9c90a
Niki <nikolett.tisza@gmail.com>
2014-09-07 19:38:50 +1200

    start fibonacci

---
+++ Assignment2/src/main/java/FibonacciSequence.java
@@ -0,0 +1,28 @@
+import acm.program.ConsoleProgram;
+
+/**
+ * Created by Niki on 07/09/2014.
+ *
+ * This program is about to display the terms in the Fibonacci sequence, starting with Fib(0)
+ and continuing as long as the terms are less than 10,000. The Fibonacci sequence
+ In the 13th century, the Italian mathematician Leonardo Fibonacci—as a way to explain
+ the geometric growth of a population of rabbits—devised a mathematical sequence that
+ now bears his name. The first two terms in this sequence, Fib(0) and Fib(1), are 0 and 1,
+ and every subsequent term is the sum of the preceding two.
+ */
+
+
+public class FibonacciSequence extends ConsoleProgram {
+
+    private static final int MAX_TERM_VALUE = 10000;
+
+        public void run() {
+
+            int fibNumber0 = 0;
+            int fibNumber1 = 1;
+            
+
+        }
+
+
+}
