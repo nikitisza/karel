50b245b778c66f24b83ada8919b596de276b843a
Niki <nikolett.tisza@gmail.com>
2014-09-18 18:42:03 +1200

    inches to centimetre

---
+++ Assignment2/src/main/java/InchToCenti.java
@@ -0,0 +1,10 @@
+import acm.program.ConsoleProgram;
+
+/**
+ * Created by Niki on 18/09/2014.
+ *
+ * this program calculates inches to centimeters
+ */
+public class InchToCenti extends ConsoleProgram {
+
+}
