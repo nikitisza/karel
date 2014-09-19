811364b3fb0885086c6b8272408d758dfc0156ad
Niki <nikolett.tisza@gmail.com>
2014-09-19 23:20:44 +1200

    update on inchToCenti

--- Assignment2/src/main/java/InchToCenti.java
+++ Assignment2/src/main/java/InchToCenti.java
@@ -7,4 +7,13 @@ import acm.program.ConsoleProgram;
  */
 public class InchToCenti extends ConsoleProgram {
 
+    public void run() {
+        println("This program converts inches to centimeter.");
+        double inches = readDouble("Enter value in inches: ");
+        double cm = inches * CENTIMETERS_PER_INCH;
+        println(inches + " inch is " + cm + " centimeter");
+
+    }
+
+    private static final double CENTIMETERS_PER_INCH = 2.54;
 }
